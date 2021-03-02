419c509aba4aa055e59077127b867c0f24495cef
Manuel Ferran Pousa <manolobiene@gmail.com>
2021-03-02 21:56:27 +0000

    Some change in lib

--- packages/lib/src/atoms/Other/Other.tsx
+++ packages/lib/src/atoms/Other/Other.tsx
@@ -4,4 +4,4 @@ import { Button, ButtonProps } from "@/atoms/Button";
 export interface OtherProps extends ButtonProps {
   foo: string;
 }
-export const Other = ({ foo }: OtherProps) => <Button>{foo}</Button>;
+export const Other = ({ foo }: OtherProps) => <Button>some change and {foo}</Button>;
